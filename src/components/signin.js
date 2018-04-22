927194589f14449eab3c28d6314d7c4f4c24b1b9
Aaron Burk <aarondburk@gmail.com>
2018-04-21 22:04:37 -0500

    fix indentation in signin.js

--- src/components/signin.js
+++ src/components/signin.js
@@ -21,30 +21,30 @@ class SignIn extends Component {
       !this.props.authenticated ?
 			<div className="popup">
 				<img src={tater} className="splash-tater">
-      <div className="popup__inner">
-				<div className="auth__title">
-					Welcome to My Note Tater
-				</div>
-				<div className="auth__sub-title">
-					you have to be a registered user to use the app
-				</div>
-        <h3>Please sign in </h3>
-        <form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
-          <fieldset>
-            <Field placeholder="username" name="username" component="input" type="text" />
-          </fieldset>
-          <fieldset>
-            <Field placeholder="password" name="password" component="input" type="password" />
-          </fieldset>
-          <button action="submit">Sign In</button>
-					<div className="auth__help-text">
-						Not yet registered?  Sign up for a free account!
+				<div className="popup__inner">
+					<div className="auth__title">
+						Welcome to My Note Tater
 					</div>
-          <NavLink to="/signup"> <button>Sign Up</button> </NavLink>
-          {this.renderAlert()}
-        </form>
-      </div>
-    </div> : null
+					<div className="auth__sub-title">
+						you have to be a registered user to use the app
+					</div>
+					<h3>Please sign in </h3>
+					<form onSubmit={handleSubmit(this.handleFormSubmit.bind(this))}>
+						<fieldset>
+							<Field placeholder="username" name="username" component="input" type="text" />
+						</fieldset>
+						<fieldset>
+							<Field placeholder="password" name="password" component="input" type="password" />
+						</fieldset>
+						<button action="submit">Sign In</button>
+						<div className="auth__help-text">
+							Not yet registered?  Sign up for a free account!
+						</div>
+						<NavLink to="/signup"> <button>Sign Up</button> </NavLink>
+						{this.renderAlert()}
+					</form>
+				</div>
+			</div> : null
     );
   }
 }
